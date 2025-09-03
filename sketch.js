6650c21bb7e80b00ea2b1971117cb8f246a8a37a
Aaron Wood <aaronw@tablebound.com>
2025-09-03 13:24:44 -0500

    not finished at all rip

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup(){
+    createCanvas(400,400);
+}
+
+function draw(){
+    background(0);
+    nostroke();
+    Fill(200,200,200);
+    ellipse(20,20,20,20);
+    ellipse(30,20,20,20);
+    ellipse(40,20,20,20);
+}
