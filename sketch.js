b53972246f6b5ec3c256f6d603f89afd8a451606
Aaron Wood <aaronw@tablebound.com>
2025-09-03 14:20:09 -0500

    less bad

--- sketch.js
+++ sketch.js
@@ -3,10 +3,26 @@ function setup(){
 }
 
 function draw(){
-    background(0);
-    nostroke();
-    Fill(200,200,200);
-    ellipse(20,20,20,20);
-    ellipse(30,20,20,20);
-    ellipse(40,20,20,20);
+    background(1);
+    fill(255);
+    noStroke();
+    ellipse(200,200,40,40);
+
+    fill(1)
+    ellipse(200,200,30,30);
+
+    fill(255)
+    ellipse(200,200,20,20);
+
+    fill(255)
+    ellipse(0,400,300,300)
+    ellipse(0,0,300,300)
+    ellipse(400,0,300,300)
+    ellipse(400,400,300,300)
+
+    fill(255)
+    ellipse(200,0,150,150)
+    ellipse(400,200,150,150)
+    ellipse(200,400,150,150)
+    ellipse(0,200,150,150)
 }
